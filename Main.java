240137ebedc001623498c8200f14a32a70e5c836
asmita06 <asmitamuthukumar@gmail.com>
2026-03-26 14:59:18 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,55 @@
-// Parent class
-class Animal {
-    void eat() {
-        System.out.println("Animal is eating");
+class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    protected void display() {
+        System.out.println("This is Calculator class");
+    }
+
+    private void show() {
+        System.out.println("Private method in Calculator");
+    }
+
+    public void accessPrivate() {
+        show();
     }
 }
 
-class Dog extends Animal {
-    void bark() {
-        System.out.println("Dog is barking");
+// separate class
+class AdvancedCalculator extends Calculator {
+
+    public int add(int a, int b) {
+        return a + b + 5;
+    }
+
+    protected void display() {
+        System.out.println("This is AdvancedCalculator class");
+    }
+
+    public void show() {
+        System.out.println("Show method in AdvancedCalculator");
     }
 }
-
 
 public class Main {
     public static void main(String[] args) {
-        Dog d = new Dog();
 
-        d.eat();   // inherited method
-        d.bark();  // child method
+        Calculator c = new Calculator();
+        System.out.println(c.add(2, 3));
+        c.display();
+        c.accessPrivate();
+
+        AdvancedCalculator ac = new AdvancedCalculator();
+        System.out.println(ac.add(2, 3));
+        ac.display();
+        ac.show();
+
+        // runtime polymorphism
+        Calculator ref = new AdvancedCalculator();
+        System.out.println(ref.add(2, 3));
+        ref.display();
+        ref.accessPrivate();
     }
 }
