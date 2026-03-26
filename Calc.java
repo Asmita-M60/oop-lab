8ee18e1eb475bef9822113c8ba9ab7ef889edd6a
asmita06 <asmitamuthukumar@gmail.com>
2026-03-26 14:48:12 +0530

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,37 @@
+class Calculator {
+
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+    protected int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    
+    private double add(double a, double b) {
+        return a + b;
+    }
+
+        public void showDecimalAddition(double a, double b) {
+        double result = add(a, b);
+        System.out.println("Sum of decimals: " + result);
+    }
+}
+
+public class Calc {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+                System.out.println("Sum of 2 integers: " + calc.add(10, 20));
+
+    
+        System.out.println("Sum of 3 integers: " + calc.add(10, 20, 30));
+
+       
+        calc.showDecimalAddition(10.5, 5.5);
+    }
+}
